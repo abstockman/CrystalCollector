29710620d428c7b7f5d7093a798d35daceae2a59
Bryce Stockman <brycestockman@gmail.com>
2017-05-18 00:09:04 -0400

    working random number generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+  var randomNumber = getRandomNumber(19,120);
+
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  $("#randomNumber").html(randomNumber);
+
   
 
 });
